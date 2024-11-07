572dd6b929a27f05c09bd9342ef89282e076a7df
Arun2728 <arun.a.addagatla@gmail.com>
2024-11-06 16:02:08 -0800

    Updated theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,19 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Lamatic</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/Lamatic/lamatic-nextra-starter-kit.git',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  // chat: {
+  //   link: 'https://lamatic.ai/docs/slack',
+  //   icon: (
+      
+  //   )
+  // },
+  docsRepositoryBase: 'https://github.com/Lamatic/lamatic-nextra-starter-kit.git',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Lamatic Docs',
   },
 }
 
